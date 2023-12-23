4c81bfbc4ced261290b2d087d506221e496cb920
Soubhagya Ranjan Sethi <soubhagya.edu12@gmail.com>
2023-12-23 20:10:12 +0530

    task1 added

---
+++ task1/index.js
@@ -0,0 +1,15 @@
+// Write your code below:
+// Write your code below:
+// Write your code below:
+// Write your code below:
+const header = document.getElementById('header');
+header.style.backgroundColor = 'green';
+header.style.borderBottom = '3px solid orange';
+const mainHeading = document.getElementById('main-heading');
+mainHeading.textContent = 'Fruit World'
+mainHeading.style.color = 'orange'
+const basketHeading = document.getElementById('basket-heading');
+basketHeading.style.color = 'green';
+
+const msg = document.getElementById('thanks');
+msg.innerHTML = '<p>Please visit us again</p>'
